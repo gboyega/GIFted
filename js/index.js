cc8059fe29a3b27fb976b5d974b85efd533ebae9
gboyega <jaiyeolagboyega@gmail.com>
2019-10-22 07:12:15 +0100

    added conditional statement to catch blank search terms and an extra if statement to distinguish errors

--- js/index.js
+++ js/index.js
@@ -5,25 +5,11 @@ var search = document.getElementById("search");
 search.addEventListener("click", () => {
     var searchText = document.getElementById("searchText").value;
     url = `https://api.giphy.com/v1/gifs/search?api_key=btcsURItKXprtrtpnmDkJtiVoTp268Q8&q=${searchText}&limit=50&offset=0&rating=G&lang=en`;
-    var request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.onload = () => {
-        if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-            var gifs = data.data;
-            content.innerHTML = "";
-            for (var i = 0; i < gifs.length; i++) {
-                displayCards(gifs, i);
-            }
-        } else {
-            window.alert("There's a problem contacting the server, Please refresh or try again in a few moments.");
-        };
-    };
-
-    request.onerror = () => {
-        window.alert("There's a problem contacting the server, Please refresh or try again in a few moments.");
+    if (searchText !== "" && searchText !== " ") {
+        getData(url);
+    } else {
+        window.alert("Please enter a search term that contains at least one alphanumeric character")
     }
-    request.send();
 })
 
 window.onload = () => {
@@ -57,8 +43,8 @@ const getData = (url) => {
 
 const displayCards = (gifs, i) => {
     var card =
-        `<div class="col-sm-2" style="margin:1em;">
-            <iframe src="${gifs[i].embed_url}" height="180" frameBorder="0" class="giphy-embed"></iframe>  
+        `<div class="col-sm-3 m-0">
+            <iframe src="${gifs[i].embed_url}" height="200px"  width:"200px" frameBorder="0"></iframe>  
         </div>`
     content.insertAdjacentHTML('beforeend', card);
 }
